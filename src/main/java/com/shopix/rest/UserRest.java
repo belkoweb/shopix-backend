0f0a84655f81fadfbe92c3902d2485b8f9e0d8d6
yassir acaf <yassir.acaf@gmail.com>
2020-09-06 17:10:11 +0100

    "last commit"

--- src/main/java/com/shopix/rest/UserRest.java
+++ src/main/java/com/shopix/rest/UserRest.java
@@ -1,6 +1,8 @@
 package com.shopix.rest;
 
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,37 +17,43 @@ import org.springframework.web.bind.annotation.RestController;
 import com.shopix.beans.User;
 import com.shopix.service.UserService;
 
+import lombok.val;
+
 @RestController
 @RequestMapping("/shopix-api/users")
 @CrossOrigin
 public class UserRest {
 	@Autowired
 	private UserService userService;
-    @GetMapping("/")
+
+	@GetMapping("/")
 	public List<User> findAll() {
-		return userService.findAll();
+		return userService.findAll().stream().filter(v -> v.getUserRole().equals("USER")).collect(Collectors.toList());
 	}
+
 	@PostMapping("/id/{id}/status/{status}")
-	public void changeStatus(@PathVariable long id , @PathVariable Boolean status) {
-		 userService.changeStatus(id, status);
+	public void changeStatus(@PathVariable long id, @PathVariable Boolean status) {
+		userService.changeStatus(id, status);
 	}
+
 	@PostMapping("/")
 	public ResponseEntity<?> save(@RequestBody User user) {
 		return userService.save(user);
 	}
+
 	@GetMapping("/nom/{nom}")
-	public User finByNom(@PathVariable("nom")String nom) {
+	public User finByNom(@PathVariable("nom") String nom) {
 		return userService.findByNom(nom);
 	}
 
-    @GetMapping("/email/{email}")
-	public User findByEmail(@PathVariable("email")String email) {
+	@GetMapping("/email/{email}")
+	public User findByEmail(@PathVariable("email") String email) {
 		return userService.findByEmail(email);
 	}
-    @GetMapping("/email/{email}/password/{password}")
-	public User findByEmailAndPassword(@PathVariable("email")String email, @PathVariable("password")String password) {
+
+	@GetMapping("/email/{email}/password/{password}")
+	public User findByEmailAndPassword(@PathVariable("email") String email, @PathVariable("password") String password) {
 		return userService.findByEmailAndPassword(email, password);
 	}
-	
 
 }
--- src/main/java/com/shopix/service/UserService.java
+++ src/main/java/com/shopix/service/UserService.java
@@ -21,4 +21,6 @@ public interface UserService {
 	
 
 	public void changeStatus(long id, Boolean status);
+	
+	
 }
